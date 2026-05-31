c7442242354d276b70c8ae521ece682c033d9a80
Nicholas Prempeh Ango <elnick247365@gmail.com>
2026-05-31 10:44:31 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 2
+#
+# Name: NICHOLAS PREMPEH ANGO
+# STUDENT ID:  2425402883
+# Email: elnick247365@gmail.com
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor, NICHOLAS PREMPEH ANG0, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials N, P, A in large stylized block letters.
 
+    print()
+    print("...NPA")
+    print()
+
+    print("...NN.........N........PPPPPPPPPP..............AAAA.........")
+    print("...NNN........N........P..........P...........A....A........")
+    print("...N.NN.......N........P..........P..........A......A.......")
+    print("...N..NN......N........P..........P.........A........A......")
+    print("...N...NN.....N........PPPPPPPPPP..........A..........A.....")
+    print("...N....NN....N........P...................AAAAAAAAAAAA.....")
+    print("...N.....NN...N........P...................A..........A.....")
+    print("...N......NN..N........P...................A..........A.....")
+    print("...N.......NNNN.****...P............****...A..........A.****")
+    print("...N........NNN.****...P............****...A..........A.****")
+    print()
 
 
 main()
